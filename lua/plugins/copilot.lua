2fd1f0370cec9928f4f3e17597b43e53e1f3592d
nemuirari <lyukey0902@gmail.com>
2026-01-15 19:06:31 +0900

    fix: copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,5 @@
 return {
   "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
   config = function()
     require("copilot").setup({
       suggestion = {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,6 @@ return {
   require("plugins.colors"),
   require("plugins.copilot"),
   require("plugins.telescope"),
-  require("plugins.terminal"),
   require("plugins.treesitter"),
   require("plugins.mason"),
   require("plugins.oil"),

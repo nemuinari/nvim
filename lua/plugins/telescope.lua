5fecfd86a087f77a4d11eb42596b8fd93a8ffeaa
nemuirari <lyukey0902@gmail.com>
2026-01-17 21:06:46 +0900

    fix: neovim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,54 +1,87 @@
-return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
-	},
-	keys = {
+-- ========================================
+-- Telescope Configuration
+-- ========================================
+
+-- ========================================
+-- Keymaps
+-- ========================================
+
+local function get_telescope_keymaps()
+	return {
 		{
-			"<leader>ff",
+			'<leader>ff',
 			function()
-				require("telescope.builtin").find_files()
+				require('telescope.builtin').find_files()
 			end,
-			desc = "Find files",
+			desc = 'Find files',
 		},
 		{
-			"<leader>fg",
+			'<leader>fg',
 			function()
-				require("telescope.builtin").live_grep()
+				require('telescope.builtin').live_grep()
 			end,
-			desc = "Grep",
+			desc = 'Live grep',
 		},
 		{
-			"<leader>fb",
+			'<leader>fb',
 			function()
-				require("telescope.builtin").buffers()
+				require('telescope.builtin').buffers()
 			end,
-			desc = "Buffers",
+			desc = 'Buffers',
 		},
 		{
-			"<leader>fh",
+			'<leader>fh',
 			function()
-				require("telescope.builtin").help_tags()
+				require('telescope.builtin').help_tags()
 			end,
-			desc = "Help tags",
+			desc = 'Help tags',
 		},
-	},
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({
-			defaults = { layout_strategy = "horizontal" },
-			extensions = {
-				file_browser = {
-					hijack_netrw = false, -- do not override netrw
-					respect_gitignore = true,
-					hidden = true,
-					grouped = true,
+	}
+end
+
+-- ========================================
+-- Telescope Setup
+-- ========================================
+
+local function get_telescope_config()
+	return {
+		defaults = {
+			layout_strategy = 'horizontal',
+			mappings = {
+				i = {
+					['<Esc>'] = function(...)
+						return require('telescope.actions').close(...)
+					end,
 				},
 			},
-		})
+		},
+		extensions = {
+			file_browser = {
+				hijack_netrw = false,
+				respect_gitignore = true,
+				hidden = true,
+				grouped = true,
+			},
+		},
+	}
+end
 
-		-- Load file browser extension
-		pcall(telescope.load_extension, "file_browser")
-	end,
+local function setup_telescope()
+	local telescope = require('telescope')
+	telescope.setup(get_telescope_config())
+	pcall(telescope.load_extension, 'file_browser')
+end
+
+-- ========================================
+-- Plugin Specification
+-- ========================================
+
+return {
+	'nvim-telescope/telescope.nvim',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-file-browser.nvim',
+	},
+	keys = get_telescope_keymaps(),
+	config = setup_telescope,
 }

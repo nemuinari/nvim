97331c1d28d67b71f72645119ceccaba83993aba
nemuirari <lyukey0902@gmail.com>
2026-03-11 20:44:50 +0900

    delete: file

--- queries/rust/injections.scm
+++
@@ -1,8 +0,0 @@
-;; extends
-(macro_invocation
-  macro: (identifier) @name (#any-of? @name "css" "style" "styled")
-  (token_tree
-    (raw_string_literal
-      (string_content) @content))
-  (#set! injection.language "css")
-  (#set! injection.content_type "declaration_list"))
